c67f556f4ce14d6326064ca9112a7f8ddb04014e
Skeleton2636 <46952604+Skeleton2636@users.noreply.github.com>
2021-05-25 12:24:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,31 @@ app = FastAPI()
 
 fakedb = []
 
-class Course(BaseModel):
-    id: int
-    name: str
-    price: float
+class Init(BaseModel):
+    integer: int
+    string: str
+    float_var: float
     boolean: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"message":"Hello klhlh"}
 
-@app.get('/courses')
+@app.get('/page')
 def get_courses():
     return fakedb
 
-@app.get('/courses/{course_id}')
+@app.get('/page/{course_id}')
 def get_course_id(course_id: int):
     course = course_id - 1
     return fakedb[course]
 
-@app.post("/courses")
-def add_course(course: Course):
+@app.post("/page")
+def add_course(course: Init):
     fakedb.append(course.dict())
     return fakedb[-1]
 
-@app.delete("/courses/{course_id}")
+@app.delete("/page/{course_id}")
 def delete_course(course_id: int):
     fakedb.pop(course_id-1)
     return {"Task": "Successful"}
